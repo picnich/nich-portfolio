2cb6395bf55ed806cc6a8212eb4276bb723ca65e
Nicholas Pereira <nicholaspereira@Nichs-Lap-2.local>
2023-11-12 21:45:21 -0500

    remove doc error

--- app/components/DarkMode/index.js
+++ app/components/DarkMode/index.js
@@ -6,22 +6,17 @@ import styles from './darkmode.module.css'
 import { Moon, Sun } from "./svg";
 
 export const DarkMode = () => {
-    // const [ doc, setDoc ] = useState(null)
-    // if (document) {
-        const [activeTheme, setActiveTheme] = useState(document.body.dataset.theme || 'light');
-        const inactiveTheme = activeTheme === "light" ? "dark" : "light";
-        
-        useEffect(() => {
-            document.body.dataset.theme = activeTheme;
-            window.localStorage.setItem("theme", activeTheme);
-        
-          }, [activeTheme]);
-    // }
-
-    // if (!activeTheme) {
-    //     return null;
-    // }
+    const [ doc, setDoc ] = useState(null)
     
+    const [activeTheme, setActiveTheme] = useState('light');
+    const inactiveTheme = activeTheme === "light" ? "dark" : "light";
+    
+    useEffect(() => {
+        document.body.dataset.theme = activeTheme;
+        window.localStorage.setItem("theme", activeTheme);
+
+    }, [activeTheme]);
+
 
     return (
         <button
