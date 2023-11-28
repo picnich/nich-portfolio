684a3b87575d4d651d9e38fad2848f32b9cfe13e
Nicholas Pereira <nicholaspereira@Nichs-Lap-2.local>
2023-11-28 00:05:27 -0500

    mistakenly hid nav

--- src/app/layout.js
+++ src/app/layout.js
@@ -55,7 +55,7 @@ export default function RootLayout({ children }) {
             __html: setInitialTheme,
           }}
         />
-        {/* <Navigation /> */}
+        <Navigation />
         {children}
         {/* <Footer /> */}
         {/* <Preloader /> */}
