e5e481b4bbbfb0cd7eebb3c6e7a6e9efde562aee
Nicholas Pereira <nicholaspereira@Nichs-Lap-2.local>
2023-11-28 09:52:13 -0500

    hid scroll true to handleCompleteAnimation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    experimental: {
+        scrollRestoration: false
+    },
+    reactStrictMode: true,
+}
 module.exports = nextConfig
